b88419acc1674c4e050c0e6d660ab6322d2e9f7a
Bill Fero <billfero@gmail.com>
2016-01-08 17:41:43 +0000

    Remove action bar. Add Robolectric and implement first test.

---
+++ app/src/test/java/com/example/calculator_app/CalculatorActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.calculator_app;
+
+import android.os.Build;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(RobolectricGradleTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.LOLLIPOP)
+public class CalculatorActivityTest {
+
+
+    private CalculatorActivity mCalculatorActivity;
+
+    @Before
+    public void setUp() throws Exception {
+        mCalculatorActivity = Robolectric.buildActivity(CalculatorActivity.class)
+                    .create()
+                    .resume()
+                    .get();
+    }
+
+    @Test
+    public void shouldNotBeNull() throws Exception {
+        assertNotNull(mCalculatorActivity);
+    }
+
+
+}
